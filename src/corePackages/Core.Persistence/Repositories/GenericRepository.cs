39574bb2dbf840ae81f126b5a382a78b6926eef6
Berkay Faruk Sarıgöz <59101018+berkayfaruk@users.noreply.github.com>
2022-02-02 00:47:25 +0300

    2.02.22

---
+++ src/corePackages/Core.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories
+{
+    public class GenericRepository<TEntity, TContext> :IGenericRepository<TEntity> where TEntity : class, new() where TContext : DbContext, new()
+    {
+        public GenericRepository(TContext context)
+        {
+            Context = context;
+        }
+
+        public TContext Context { get; }
+
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
